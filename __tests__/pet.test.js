1acfa9a2b500fdce1e8b8f4853d7ca1b1222c80e
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-24 23:17:01 +0100

    added tests for different fitness levels after calling walk method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,9 @@
 const TestRunner = require("jest-runner");
 
 
-const Pet = require('../src/pet');
+const Pet  = require('../src/pet');
+const maxFitness = require('../src/pet');
+
 /* describe("check that the variable pet has an assignment"){
     expect(pet).toBe(true);
 }
@@ -30,19 +32,39 @@ describe('constructor', () => {
     expect(youngPet.age).toEqual(1);
   }) ;
 
-  it('increases hunger by 5 when growUp is called and fitness to decrease by 3', () =>
+  it('increases hunger when growUp is called and fitness to decrease', () =>
   {
     const hungryPet = new Pet('hungry');
     hungryPet.growUp();
     expect(hungryPet.hunger).toEqual(5);
     expect(hungryPet.fitness).toEqual(7);
-  });
-
-  
+    
+    
     const pet2 = new Pet('hungry');
     pet2.growUp(2);
     expect(pet2.hunger).toEqual(10);
     expect(pet2.fitness).toEqual(4);
-  
+  });
+
+  it('gains fitness when calling walk if fitness is less than 10', () =>{
+    const unfitPet = new Pet('unfit');
+    unfitPet.fitness = 1;
+    unfitPet.walk();
+    expect(unfitPet.fitness).toBeGreaterThan(1);
+  });
+
+  it('stays at fitness 10 on walk if already at max', () => {
+    const fitPet = new Pet('fit');
+    fitPet.fitness = 10; 
+    fitPet.walk();
+    expect(fitPet.fitness).toEqual(10);
+  });
+
+  it("doesn't go above fitness of 10", () => {
+const fittishPet = new Pet('fittish');
+fittishPet.fitness = 9;
+fittishPet.walk();
+expect(fittishPet.fitness).toBeLessThanOrEqual(10);
+  });
 
 });
--- src/pet.js
+++ src/pet.js
@@ -1,18 +1,25 @@
-
+let hungerIncreaser = 5;
+let fitnessIncreaser = 3;
+let walkFitnessIncreaser = 4;
+let maxFitness = 10;
 
 function Pet(name) {
-
-  return  { name: name,
+    return {name: name,
             age: 0,
             hunger: 0,
             fitness: 10,
             growUp: function(increase = 1) {
             this.age += increase;
-            this.hunger += increase * 5;
-            this.fitness -= increase * 3;
-            return;
+            this.hunger += increase * hungerIncreaser;
+            this.fitness -= increase * fitnessIncreaser;},
+            walk: function(session = 1){
+            this.fitness += session * walkFitnessIncreaser;
+            if (this.fitness > maxFitness){this.fitness = maxFitness;
+            }
+          }
+
             }};
-};
+
 
 const bobby = new Pet('Bobby');
 const dobby = new Pet('Dobby');
