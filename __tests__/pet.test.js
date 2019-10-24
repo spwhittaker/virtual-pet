9cd6bdb68fe2705f809da5ccf318fbc4725e91c5
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-24 20:57:05 +0100

    added growUp test and method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,6 +14,20 @@ describe('constructor', () => {
 
   it('gives an object including the name passed in', () =>
   { const myPet = new Pet('fido');
-    expect(myPet.name).toEqual('fido')})
+    expect(myPet.name).toEqual('fido')});
+
+  it('starts age at 0', () =>
+  {
+    const testPet = new Pet('testBoi');
+  expect(testPet.age).toEqual(0);
+
+  });
+
+  it('increases its age to 1 after growUp', () =>
+  {
+    const youngPet = new Pet('oldBoi');
+    youngPet.growUp();
+    expect(youngPet.age).toEqual(1);
+  }) 
 
 });
--- src/pet.js
+++ src/pet.js
@@ -2,8 +2,18 @@
 
 function Pet(name) {
 
-  return  {name: name};
+  return  { name: name,
+            age: 0,
+            growUp: function() {
+            return this.age ++;
+            }};
 };
 
+const bobby = new Pet('Bobby');
+const dobby = new Pet('Dobby');
+console.log(bobby);
+bobby.growUp();
+console.log(bobby);
+console.log(dobby);
 
 module.exports = Pet;
